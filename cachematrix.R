1cb0701dd20be323799272d6a4269ccba0ffe59f
Norm Cho <normancho@gmail.com>
2017-12-16 16:20:34 -0600

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 2 seperate functions 
+## This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+## Writing matrix function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+              m <- NULL
+              
+## Setting up matrix
+              set <- function(y) {
+                x <<- y
+                m <<- NULL
+              }
+## Getting matrix
+              get <- function() x
+## Setting the inverse
+              setInverse <- function(solve) m <<- solve
+## Getting the inverse
+              getInverse <- function() m
+## List of objects
+              list(set = set, get = get,
+                   setInverse = setInverse,
+                   getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Matrix that returns the inverse(solve) of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+## If the data is cached or "stored", then returns message
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+## If no cached value, then getting the values set in x  
+  data <- x$get()
+  
+## Calculating the inverse via solve
+  m <- solve(data, ...)
+  x$setInverse(m)
+  
+## The values of the inverse matrix
+  m
 }
+
+## Testing to see whether the code works
+## x <- matrix(data=1:4, nrow=2, ncol=2)  -- A simple 2 X 2 matrix
+## foo <- makeCacheMatrix(x) -- Creating a variable that incorporates the values from x and the 
+## makeCachematrix function
+## bar <- cacheSolve(foo) --To see the inverse data values,then enter bar to the inverted values
+
+## bar <- cacheSolve(foo) --Running it again to see if the "getting cached data" appears
+
+## Would recommend understanding the math of inverse of a matrix to help to verify the code
